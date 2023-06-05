a1f41cf7107be6456cedd19871945b7144fd524c
Rasp <s25818@st.pans.nysa.pl>
2023-06-05 10:14:48 +0200

    Byle jakie demo

--- src/controller/result.controller.ts
+++ src/controller/result.controller.ts
@@ -28,7 +28,7 @@ export class ResultController {
   @Post('result')
   async getDataAndSend(@Body() FormData: PlantFormData, @Res() res) {
     data = [];
-    console.log("Informacja od uzytkownika: "  + FormData.color);
+   // console.log("Informacja od uzytkownika: "  + FormData.color);
     await this.resultService.emitEvent('clientData', FormData);
     zmienna = async () => {
       await res.render('result', { data })
@@ -45,11 +45,11 @@ export class ResultController {
         agents.push(temp);
         await this.negotiationClient(agents[i]);
         functions.push(questionToClient = async (plant) => {
-          console.log("client ma"+plant)
+          //console.log("client ma"+plant)
           await agents[i].getPlanttoClient(plant);
         })
         i++;
-      }while(i < 7);
+      }while(i < 2000);
       i = 0;
       do{
         let temp = new ShopAgent();
@@ -57,9 +57,9 @@ export class ResultController {
         shopAgents.push(temp);
         await this.negotiationShop(shopAgents[i], functions[i]);
         i++;
-      }while(i < 7);
+      }while(i < 2000);
 
-    console.log('Otrzymano dane od klienta:', formData);
+    //console.log('Otrzymano dane od klienta:', formData);
 
     const response = {
         message: 'Odpowiedź od sprzedawcy',
@@ -79,7 +79,7 @@ export class ResultController {
   async negotiationShop(agent, questionToClient){
     const randomNum = Math.floor(Math.random() * (21)) + 1;
     await this.resultService.findOnePlant(randomNum).then(result => {
-      console.log(result)
+      //console.log(result)
       questionToClient(result)
       })
       questionToShop = async () => {
@@ -88,7 +88,7 @@ export class ResultController {
 
   async sendDataToUser() {
     this.resultService.onEvent('sellerResponse', async(response) => {
-        await console.log('Otrzymano dane od sprzedawcy:', response);
+        //console.log('Otrzymano dane od sprzedawcy:', response);
         let i = 0, k = 0;
         do{
           const plantToShow = await agents[i].getPlanttoShow();
@@ -99,9 +99,9 @@ export class ResultController {
                 k++;
             }
             i++;
-            console.log(k);
-            console.log(i);
-            console.log("Tutaj: " + data[i-k]);
+            //console.log(k);
+            //console.log(i);
+            //console.log("Tutaj: " + data[i-k]);
           } else {
             await new Promise(resolve => setTimeout(resolve, 100));
           }
